0cb4baccfd7d0f754eb88a3adde63774e9f078a5
Ivan <30-02@mail.ru>
2024-04-09 06:33:37 +0000

    Закрыть прием реестров

--- app/Policies/MessagePolicy.php
+++ app/Policies/MessagePolicy.php
@@ -30,15 +30,15 @@ class MessagePolicy
         if($messageType === NULL){
             return true;
         }
-        /*
+        /**/
         if($messageType === 'reg') {
-            if (time() > strtotime('2024-04-07 19:00')) { // KGN -5
-                if ($user->organization->id !== 9) {  // ГБУ "Курганская поликлиника №2"
+            if (time() > strtotime('2024-04-09 19:00')) { // KGN -5
+                //if ($user->organization->id !== 9) {  // ГБУ "Курганская поликлиника №2"
                     return Response::deny('Прием реестров закрыт');
-                }
+                //}
             }
         }
-        */
+        
         
         if($user->hasPermissionTo('send '.$messageType)){
             return true;
