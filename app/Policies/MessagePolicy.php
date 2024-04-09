b99e049f43fd2a6967ec8050b14d8ba80bb6c640
Ivan <30-02@mail.ru>
2024-04-09 09:21:58 +0000

    Прием реестров закрыт до мая

--- app/Policies/MessagePolicy.php
+++ app/Policies/MessagePolicy.php
@@ -32,7 +32,7 @@ class MessagePolicy
         }
         /**/
         if($messageType === 'reg') {
-            if (time() > strtotime('2024-04-09 19:00')) { // KGN -5
+            if (time() < strtotime('2024-05-01 00:00')) { // KGN -5
                 //if ($user->organization->id !== 9) {  // ГБУ "Курганская поликлиника №2"
                     return Response::deny('Прием реестров закрыт');
                 //}
