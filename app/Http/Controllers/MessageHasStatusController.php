24d8171927a2cba9de62e9aa66a379e67941a3a0
Ivan <30-02@mail.ru>
2024-06-07 18:59:54 +0000

    полномочия на установку статуса дисп листа информационной системой-получателем

--- app/Http/Controllers/MessageHasStatusController.php
+++ app/Http/Controllers/MessageHasStatusController.php
@@ -99,6 +99,17 @@ class MessageHasStatusController extends Controller
                 return new MessageStatusResource($status);
         }
 
+        if(
+            ($request->status == 'rejected_flc' || $request->status == 'in_progress' || $request->status == 'loaded' /*|| $request->status == 'signed_mo'*/)
+            && $msg->type->name == 'displist'
+            && $user->hasPermissionTo('auto-set-status displist')
+        ){
+                $status = MessageStatus::where('name',$request->status)->firstOrFail();
+                $msg->status_id = $status->id;
+                $msg->save();
+                return new MessageStatusResource($status);
+        }
+
         if($request->status == 'sent' &&
            $msg->type->name == 'displist' &&
            $msg->status->name == 'draft' &&
